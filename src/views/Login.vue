6f8fc4a1505aecd7f3b9959d47d681643f5e1433
MikeLouieBania <tamamopasta@gmail.com>
2022-09-15 21:56:36 +0800

    m

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,7 +9,7 @@
   
 <script setup>
     import { getAuth, createUserWithEmailAndPassword } from '@firebase/auth';
-    import { ref } from 'vue'
+    import { ref } from 'vue';
     import { useRouter } from 'vue-router';
 
     const email = ref ("");
