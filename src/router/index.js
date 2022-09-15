39c1c8b87430990956b00929789306590aedb2ac
MikeLouieBania <tamamopasta@gmail.com>
2022-09-16 03:44:35 +0800

    n

--- src/router/index.js
+++ src/router/index.js
@@ -87,7 +87,7 @@ const getCurrentUser = () => {
   })
 }
 
-router.beforeEach(async(to, from, next) => {
+router.beforeEach((to, from, next) => {
   if(to.matched.some((record) => record.meta.requiresAuth)) {
     if((getAuth) .getCurrentUser) {
       next();
